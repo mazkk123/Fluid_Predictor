aed34ed505d86b2d81b4e8399b9c4d9492ae5470
mazkk123 <91699089+mazkk123@users.noreply.github.com>
2023-08-14 00:01:06 +0100

    added prediction step

--- python/Fluid_Calculations/compute_PCSPH.py
+++ python/Fluid_Calculations/compute_PCSPH.py
@@ -53,50 +53,56 @@ class PCSPH(SPH):
 
     # ---------------------------------------------------------------------- PREDICT DENSITY -----------------------------------------------------------------------------
 
-    def update_del_x(self):
-        pressure_const = 2*self.particle.pressure_correction / m.pow(self.PARAMETERS["mass_density"], 2)
-        constant = -1*m.pow(self.delta_time, 2)*self.particle.mass*pressure_const
+    def update_del_x(self, particle):
+        pressure_const = 2*particle.pressure_correction / m.pow(self.PARAMETERS["mass_density"], 2)
+        constant = -1*m.pow(self.delta_time, 2)*particle.mass*pressure_const
 
-        for nbr_particle in self.neighbours_list:
-            self.particle.delta_x += (
+        for nbr_particle in particle.neighbour_list:
+            particle.delta_x += (
                 self.cubic_spline_kernel_gradient(self.particle.predicted_initial_pos - nbr_particle.initial_pos)
             )
-        self.particle.delta_x *= constant
+        particle.delta_x *= constant
 
-    def update_density_change(self):
+    def update_density_change(self, particle):
         sum_del_neighbour_x, sum_weights = 0, 0
-        self.particle.density_change = 0
+        particle.density_change = 0
 
-        for nbr_particle in self.neighbours_list:
+        for nbr_particle in particle.neighbour_list:
             sum_weights += (
-                self.cubic_spline_kernel_gradient(self.particle.predicted_initial_pos - 
-                                                  nbr_particle.initial_pos)
+                self.cubic_spline_kernel_gradient(particle.predicted_initial_pos - 
+                                                  nbr_particle.predicted_initial_pos)
             )
             sum_del_neighbour_x += (
-                self.cubic_spline_kernel_gradient(self.particle.predicted_initial_pos - 
-                                                  nbr_particle.initial_pos)*nbr_particle.delta_x
+                self.cubic_spline_kernel_gradient(particle.predicted_initial_pos - 
+                                                  nbr_particle.predicted_initial_pos)*nbr_particle.delta_x
             )
-        self.particle.density_change = self.particle.mass*(self.particle.delta_x*sum_weights - sum_del_neighbour_x)
+        particle.density_change = self.particle.mass*(self.particle.delta_x*sum_weights - sum_del_neighbour_x)
 
     def update_next_density(self):
-        self.update_del_x()
-        self.update_density_change()
+        
+        for nbr in self.neighbours_list:
+            self.update_del_x(nbr)
+            self.update_density_change(nbr)
+            
+        self.update_del_x(self.particle)
+        self.update_density_change(self.particle)
+        
         self.particle.predicted_density += self.particle.density_change
 
-    def calculate_density_error(self):
-        return self.particle.predicted_density - self.PARAMETERS["mass_density"]
+    def calculate_density_error(self, particle):
+        return particle.predicted_density - self.PARAMETERS["mass_density"]
 
     # -------------------------------------------------------------------- COMPUTE PRESSURE -----------------------------------------------------------------------------
 
-    def find_kronecker_delta(self):
+    def find_kronecker_delta(self, particle):
         beta_value = self.find_beta_const()
         accum_denom, denom = 0, 0
-        for nbr_particle in self.neighbours_list:
+        for nbr_particle in particle.neighbour_list:
             accum_denom += (
-                m.pow(self.cubic_spline_kernel_gradient(self.particle.predicted_initial_pos - nbr_particle.initial_pos), 2)
+                m.pow(self.cubic_spline_kernel_gradient(particle.predicted_initial_pos - nbr_particle.predicted_initial_pos), 2)
             )
             denom += (
-                self.cubic_spline_kernel_gradient(self.particle.predicted_initial_pos - nbr_particle.initial_pos)
+                self.cubic_spline_kernel_gradient(particle.predicted_initial_pos - nbr_particle.predicted_initial_pos)
             )
 
         try:
@@ -111,7 +117,10 @@ class PCSPH(SPH):
         return kronecker
 
     def update_pressure_correction(self):
-        self.particle.pressure_correction += self.find_kronecker_delta()*self.calculate_density_error()
+        for nbr in self.neighbours_list:
+           nbr.pressure_correction += self.find_kronecker_delta(nbr)*self.calculate_density_error(nbr)
+            
+        self.particle.pressure_correction += self.find_kronecker_delta(self.particle)*self.calculate_density_error(self.particle)
     
     def update_pressure(self):
         self.particle.pressure = (
@@ -162,6 +171,28 @@ class PCSPH(SPH):
         """
         particle.gravity = self.gravity_const
     
+    def update_normal_field(self, particle):
+        """
+        """
+        normal_field = np.array([0, 0, 0],  dtype="float64")
+        for id, nbr_particle in enumerate(particle.neighbour_list):
+            pos_difference = particle.initial_pos - nbr_particle.initial_pos
+            normal_field += (
+                particle.mass * 1/particle.mass_density * self.kernel_gradient(pos_difference, 0)
+            )
+        return normal_field
+
+    def update_surface_curvature(self, particle):
+        """        
+        """
+        surface_curvature = 0
+        for id, nbr_particle in enumerate(particle.neighbour_list):
+            surface_curvature += (
+                particle.mass * 1/particle.mass_density * self.kernel_laplacian(
+                particle.initial_pos - nbr_particle.initial_pos, 0)
+            )
+        return surface_curvature
+    
     def update_surface_tension(self, particle):
         """
         """
@@ -183,7 +214,7 @@ class PCSPH(SPH):
     
     def update_predicted_attrs(self):
         
-        for particle in self.all_particles:
+        for particle in self.neighbours_list:
             self.update_mass_density(particle)
             self.update_gravity(particle)
             self.update_surface_tension(particle)
@@ -199,6 +230,8 @@ class PCSPH(SPH):
 
             particle.predicted_velocity = particle.velocity + self.delta_time*particle.acceleration
             particle.predicted_initial_pos = particle.predicted_velocity + particle.predicted_velocity*self.delta_time
+            
+        self.particle.predicted_density = self.particle.mass_density
 
     def update_all_forces(self):
         
@@ -224,19 +257,21 @@ class PCSPH(SPH):
 
         self.update_predicted_attrs()
 
-        while (self.calculate_density_error() > 0.1*self.PARAMETERS["mass_density"]) or \
+        while (self.calculate_density_error(self.particle) > 0.1*self.PARAMETERS["mass_density"]) or \
             iterations < self.OTHER_PARAMS["max_iterations"]:
 
             self.update_pressure_correction()
             self.update_next_density()
             
             iterations += 1
-
+        
+        self.particle.mass_density = self.particle.predicted_density
         self.update_all_forces()
 
         self.particle.acceleration = self.all_forces / self.particle.mass
 
         self.choose_time_stepping()
-        self.adapt_to_CFL()        
+        #self.adapt_to_CFL()        
+        
         self.XSPH_vel_correction()
         self.choose_collision_types()
