e4970aa5088aa8b223256c1540679be533b1ae93
mazkk123 <91699089+mazkk123@users.noreply.github.com>
2023-08-17 13:18:30 +0100

    Updated neighbour errors

--- python/Fluid_Calculations/compute_DFSPH.py
+++ python/Fluid_Calculations/compute_DFSPH.py
@@ -295,16 +295,19 @@ class DFSPH(SPH):
 
         divergence_velocity = np.array([0, 0, 0], dtype="float64")
         for nbr_particle in self.particle.neighbour_list:
-            divergence_velocity += (
-                nbr_particle.mass *
-                (
-                    (self.particle.stiffness_k_v / self.particle.mass_density) +
-                    (nbr_particle.stiffness_k_v / nbr_particle.mass_density)
-                ) *
-                self.cubic_spline_kernel_gradient(
-                    self.particle.initial_pos - nbr_particle.initial_pos
+            try:
+                divergence_velocity += (
+                    nbr_particle.mass *
+                    (
+                        (self.particle.stiffness_k_v / self.particle.mass_density) +
+                        (nbr_particle.stiffness_k_v / nbr_particle.mass_density)
+                    ) *
+                    self.cubic_spline_kernel_gradient(
+                        self.particle.initial_pos - nbr_particle.initial_pos
+                    )
                 )
-            )
+            except (ValueError, TypeError):
+                divergence_velocity += np.array([0, 0, 0], dtype="float64")
         self.particle.predicted_velocity -= (
             self.delta_time * divergence_velocity
         )
